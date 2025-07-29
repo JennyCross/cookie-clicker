9f2ce662e097048bf3239f9243dd474727df7591
JennyCross <jjx1972@gmail.com>
2025-07-29 08:45:51 +0100

    Add timeline files

---
+++ target/cucumber/timeline/report.js
@@ -0,0 +1,274 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "8c38e256-4efb-4f08-a7be-ebd54951a155",
+    "feature": "Cookie Clicker",
+    "scenario": "Julie clicks the cookie button 15 times",
+    "start": 1753775044310,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1753775045514,
+    "className": "passed"
+  },
+  {
+    "id": "abfce81c-b527-49b3-9e87-e09e22cd8036",
+    "feature": "Sell Cookies",
+    "scenario": "Julie clicks the cookie button 15 times and sells 3 cookie(s)",
+    "start": 1753775059674,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1753775061171,
+    "className": "passed"
+  },
+  {
+    "id": "2793b33c-d7c0-4cc1-b913-f9b97fae5a8e",
+    "feature": "Cookie Clicker",
+    "scenario": "Gill clicks the cookie button 1 times",
+    "start": 1753775041731,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1753775042444,
+    "className": "passed"
+  },
+  {
+    "id": "91f50e42-cc16-4c41-97ce-2380665b6cc0",
+    "feature": "Sell Cookies",
+    "scenario": "Heather clicks the cookie button 30 times and sells -1 cookie(s)",
+    "start": 1753775074021,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1753775080752,
+    "className": "failed"
+  },
+  {
+    "id": "3d4986f8-a117-41bb-bdb0-8d6a68b6429e",
+    "feature": "Sell Cookies",
+    "scenario": "Bob clicks the cookie button 5 times and sells 1 cookie(s)",
+    "start": 1753775057561,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1753775058446,
+    "className": "passed"
+  },
+  {
+    "id": "1bb1d6ca-9267-4d47-8093-18f0c8a86d45",
+    "feature": "Buy Factories",
+    "scenario": "Gill clicks the cookie button 1 times and buys 1 factories",
+    "start": 1753774984029,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1753774992442,
+    "className": "passed"
+  },
+  {
+    "id": "159f8cf0-c9eb-4129-bb53-3c5a0180284a",
+    "feature": "Sell Cookies",
+    "scenario": "Frank clicks the cookie button 10 times and sells 2 cookie(s)",
+    "start": 1753775058447,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1753775059672,
+    "className": "passed"
+  },
+  {
+    "id": "1a6028b8-050c-4fa6-be15-a1136b545a16",
+    "feature": "Buy Factories",
+    "scenario": "Julie clicks the cookie button 15 times and buys 1 factories",
+    "start": 1753775007526,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1753775016180,
+    "className": "passed"
+  },
+  {
+    "id": "4770fd83-8362-43e5-abb0-297b988e1db6",
+    "feature": "Cookie Clicker",
+    "scenario": "Mary clicks the cookie button 25 times",
+    "start": 1753775046865,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1753775048377,
+    "className": "passed"
+  },
+  {
+    "id": "b0c20d71-6d79-4e02-bd06-495d5ae2f11a",
+    "feature": "Buy Factories",
+    "scenario": "Spencer clicks the cookie button 20 times and buys 2 factories",
+    "start": 1753775016183,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1753775023760,
+    "className": "passed"
+  },
+  {
+    "id": "015c5c86-73ee-4aab-bc9f-e1c2795c953e",
+    "feature": "Cookie Clicker",
+    "scenario": "Spencer clicks the cookie button 20 times",
+    "start": 1753775045517,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1753775046863,
+    "className": "passed"
+  },
+  {
+    "id": "5320971d-0f7a-4669-886a-e9a3402c52bc",
+    "feature": "Cookie Clicker",
+    "scenario": "Heather clicks the cookie button 30 times",
+    "start": 1753775048379,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1753775050059,
+    "className": "passed"
+  },
+  {
+    "id": "e7a6299b-cafb-46de-89d1-0d673527bd3c",
+    "feature": "Sell Cookies",
+    "scenario": "Gill clicks the cookie button 1 times and sells 1 cookie(s)",
+    "start": 1753775051906,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1753775057559,
+    "className": "failed"
+  },
+  {
+    "id": "f8362833-fc13-472d-8bd8-89164dd2592d",
+    "feature": "Cookie Clicker",
+    "scenario": "Bob clicks the cookie button 5 times",
+    "start": 1753775042446,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1753775043312,
+    "className": "passed"
+  },
+  {
+    "id": "48ae1846-677b-45d8-a4f3-c94c34e6f205",
+    "feature": "Cookie Clicker",
+    "scenario": "Jason clicks the cookie button 35 times",
+    "start": 1753775050062,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1753775051904,
+    "className": "passed"
+  },
+  {
+    "id": "7dd29f6e-a75f-4ff8-8884-d7ac4f717d75",
+    "feature": "Buy Factories",
+    "scenario": "Bob clicks the cookie button 5 times and buys 1 factories",
+    "start": 1753774992448,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1753775000024,
+    "className": "passed"
+  },
+  {
+    "id": "66abd910-1d75-4ea3-99f2-c8b39e9e87b1",
+    "feature": "Cookie Clicker",
+    "scenario": "Frank clicks the cookie button 10 times",
+    "start": 1753775043314,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1753775044308,
+    "className": "passed"
+  },
+  {
+    "id": "e93f48e6-ca86-4675-becc-1e2b9e2866f4",
+    "feature": "Sell Cookies",
+    "scenario": "Mary clicks the cookie button 25 times and sells 26 cookie(s)",
+    "start": 1753775067567,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1753775074019,
+    "className": "failed"
+  },
+  {
+    "id": "50ab4906-b6d6-4c73-9585-23b4684a2932",
+    "feature": "Sell Cookies",
+    "scenario": "Jason clicks the cookie button 35 times and sells B cookie(s)",
+    "start": 1753775080754,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1753775082489,
+    "className": "failed"
+  },
+  {
+    "id": "d83c40d6-9748-4c3c-8f63-e12dd6bb3c5c",
+    "feature": "Buy Factories",
+    "scenario": "Frank clicks the cookie button 10 times and buys 1 factories",
+    "start": 1753775000027,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1753775007523,
+    "className": "passed"
+  },
+  {
+    "id": "c8648465-9dab-439e-93dc-6e8afda029f9",
+    "feature": "Buy Factories",
+    "scenario": "Jason clicks the cookie button 35 times and buys C factories",
+    "start": 1753775039419,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1753775041729,
+    "className": "failed"
+  },
+  {
+    "id": "9c33579d-aecf-4506-bfc7-5c4b3ec2f63d",
+    "feature": "Sell Cookies",
+    "scenario": "Spencer clicks the cookie button 20 times and sells 4 cookie(s)",
+    "start": 1753775061173,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1753775067565,
+    "className": "failed"
+  },
+  {
+    "id": "500b0dba-77d5-4cef-93d1-e1bd32bbd8b0",
+    "feature": "Buy Factories",
+    "scenario": "Heather clicks the cookie button 30 times and buys -3 factories",
+    "start": 1753775032222,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1753775039414,
+    "className": "failed"
+  },
+  {
+    "id": "9d0c01c7-c901-4b98-89d2-56ee3fe39b4a",
+    "feature": "Buy Factories",
+    "scenario": "Mary clicks the cookie button 25 times and buys 2 factories",
+    "start": 1753775023763,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1753775032220,
+    "className": "passed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 1,
+    "content": "Thread[#1,main,5,main]"
+  }
+]);
+});
